0bdf523d471fb6e6917ae1f4b5c225e7a9c5ef3f
mwoodpatrick <mwoodpatrick@gmail.com>
2021-12-18 11:45:13 -0800

    Add node & express apps

---
+++ packages/my-express-app/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'my-express-app',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/my-express-app',
+};
---
+++ packages/my-node-app/src/main.ts
@@ -0,0 +1,2 @@
+require('@my-payload/complex');
+console.log('Hello World from my-node-app xxx!');
